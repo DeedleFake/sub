f96496588692486a112ce1cb8aaa80e45ccd44b5
DeedleFake <deedlefake@users.noreply.github.com>
2019-07-22 12:06:01 -0400

    A few API changes.

--- sub.go
+++ sub.go
@@ -13,10 +13,29 @@ import (
 
 // A Commander controls a set of subcommands.
 type Commander struct {
+	// Output is the location to which output is written. Defaults to
+	// os.Stderr.
+	Output io.Writer
+
+	// Help is text displayed when the help command is run without any
+	// arguments.
+	Help string
+
+	// Flags is a function that is called to populate the global
+	// FlagSet. If it is non-nil, then it is assumed that there are
+	// global flags, which changes some text formatting.
+	Flags func(*flag.FlagSet)
+
 	name     string
-	help     string
-	flags    func(*flag.FlagSet)
 	commands []Command
+}
+
+func (c *Commander) output() io.Writer {
+	if c.Output == nil {
+		return os.Stderr
+	}
+
+	return c.Output
 }
 
 // Register registers a command with the Commander. If a command with
@@ -43,19 +62,6 @@ func (c *Commander) get(name string) Command {
 	return nil
 }
 
-// Help sets the help message. This should be a longer help message
-// that explains what the program.
-func (c *Commander) Help(help string) {
-	c.help = help
-}
-
-// Flags sets flags as the function to use to fill the global FlagSet.
-// If any function is set at all, it is assumed that there are global
-// flags, which changes the structure of the global help message.
-func (c *Commander) Flags(flags func(*flag.FlagSet)) {
-	c.flags = flags
-}
-
 // Run runs the commander against the given arguments. The first
 // argument should be the name of the executable. In many cases, this
 // should be filepath.Base(os.Args[0]).
@@ -68,10 +74,10 @@ func (c *Commander) Run(args []string) error {
 
 	fset := flag.NewFlagSet(args[0], flag.ContinueOnError)
 	fset.Usage = func() {
-		_ = (*helpCmd)(c).Run(nil)
+		_ = c.HelpCmd().Run(nil)
 	}
-	if c.flags != nil {
-		c.flags(fset)
+	if c.Flags != nil {
+		c.Flags(fset)
 	}
 	err := fset.Parse(args[1:])
 	if err != nil {
@@ -85,14 +91,14 @@ func (c *Commander) Run(args []string) error {
 
 	cmd := c.get(fset.Arg(0))
 	if cmd == nil {
-		fmt.Fprintf(os.Stderr, "Error: No such command: %q\n\n", fset.Arg(0))
+		fmt.Fprintf(c.output(), "Error: No such command: %q\n\n", fset.Arg(0))
 		fset.Usage()
 		return flag.ErrHelp
 	}
 
 	sub := flag.NewFlagSet(cmd.Name(), flag.ContinueOnError)
 	sub.Usage = func() {
-		_ = (*helpCmd)(c).Run([]string{cmd.Name()})
+		_ = c.HelpCmd().Run([]string{cmd.Name()})
 	}
 	cmd.Flags(sub)
 	err = sub.Parse(fset.Args()[1:])
@@ -128,13 +134,15 @@ type Command interface {
 	Run(args []string) error
 }
 
-type helpCmd Commander
+type helpCmd struct {
+	*Commander
+}
 
 // HelpCmd returns a "help" Command that provides help for c. If
 // clients want an explicit "help" command to be available, this must
 // be manually registered.
 func (c *Commander) HelpCmd() Command {
-	return (*helpCmd)(c)
+	return &helpCmd{c}
 }
 
 func (h *helpCmd) Name() string {
@@ -156,45 +164,44 @@ func (h *helpCmd) Flags(*flag.FlagSet) {
 }
 
 func (h *helpCmd) Run(args []string) error {
-	c := (*Commander)(h)
 	if len(args) == 0 {
-		name := c.name
+		name := h.name
 		if name == "" {
 			name = filepath.Base(os.Args[0])
 		}
 
 		globalOptions := ""
-		if c.flags != nil {
+		if h.Commander.Flags != nil {
 			globalOptions = " [global options]"
 		}
 
-		fmt.Fprintf(os.Stderr, "Usage: %v%v <subcommand> [subcommand arguments]\n", name, globalOptions)
-		if c.help != "" {
-			fmt.Fprintf(os.Stderr, "\n%v\n", strings.TrimSpace(c.help))
+		fmt.Fprintf(h.output(), "Usage: %v%v <subcommand> [subcommand arguments]\n", name, globalOptions)
+		if h.Commander.Help != "" {
+			fmt.Fprintf(h.output(), "\n%v\n", strings.TrimSpace(h.Commander.Help))
 		}
-		if c.flags != nil {
-			fmt.Fprintf(os.Stderr, "\nGlobal Options:\n")
+		if h.Commander.Flags != nil {
+			fmt.Fprintf(h.output(), "\nGlobal Options:\n")
 			fset := flag.NewFlagSet(name, flag.ContinueOnError)
-			c.flags(fset)
+			h.Commander.Flags(fset)
 			fset.PrintDefaults()
 		}
-		fmt.Fprintf(os.Stderr, "\nCommands:\n")
-		for _, cmd := range c.commands {
-			fmt.Fprintf(os.Stderr, "\t%v\t\t%v\n", cmd.Name(), cmd.Desc())
+		fmt.Fprintf(h.output(), "\nCommands:\n")
+		for _, cmd := range h.commands {
+			fmt.Fprintf(h.output(), "\t%v\t\t%v\n", cmd.Name(), cmd.Desc())
 		}
 
 		return nil
 	}
 
-	cmd := c.get(args[0])
+	cmd := h.get(args[0])
 	if cmd == nil {
-		fmt.Fprintf(os.Stderr, "Error: No such command: %q\n\n", args[0])
+		fmt.Fprintf(h.output(), "Error: No such command: %q\n\n", args[0])
 		_ = h.Run(nil)
 		return flag.ErrHelp
 	}
 
 	if cmd.Help() != "" {
-		fmt.Fprintf(os.Stderr, "%v\n", strings.TrimSpace(cmd.Help()))
+		fmt.Fprintf(h.output(), "%v\n", strings.TrimSpace(cmd.Help()))
 	}
 
 	var optionBuf bytes.Buffer
@@ -203,8 +210,8 @@ func (h *helpCmd) Run(args []string) error {
 	cmd.Flags(fset)
 	fset.PrintDefaults()
 	if optionBuf.Len() > 0 {
-		fmt.Fprintf(os.Stderr, "\nOptions:\n")
-		_, _ = io.Copy(os.Stderr, &optionBuf)
+		fmt.Fprintf(h.output(), "\nOptions:\n")
+		_, _ = io.Copy(h.output(), &optionBuf)
 	}
 
 	return nil
